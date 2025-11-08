c618892f74111542c215d0a9aecd1b8bdcdf0cb1
Richard Phan <richard.phan626@gmail.com>
2025-11-07 16:15:52 -0800

    Add files via upload

---
+++ prac5.py
@@ -0,0 +1,13 @@
+#11/7/25
+
+#create a function that calculates volume (b*w*h)
+#return that value and print it
+
+def volume(b,w,h):
+    height = b*w*h
+    return height
+
+
+
+print(volume(1,2,3))
+
