c995a5e34590e511832865318f9bf9247740132e
Richard Phan <richard.phan626@gmail.com>
2025-11-12 16:08:51 -0800

    Add files via upload

--- Practice/type.py
+++ Practice/type.py
@@ -1,1 +1,19 @@
+# Richard Phan
+# 11/12/25
 
+def main():
+    pokemon = input("Enter a starter pokemon from gen 1: ")
+    type(pokemon)
+
+def type(name):
+    match name:
+        case "Charmander":
+            print("Fire type pokemon")
+        case "Squirtle":
+            print("Water type pokemon")
+        case "Bulbasaur":
+            print("Grass type pokemon")
+        case _:
+            print("I don't recognize that type")
+
+    main()
